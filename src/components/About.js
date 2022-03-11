32bcd80f6def887837f325372c5cdfaceb2e5889
Ramya Parimi <33761166+ramyaparimi@users.noreply.github.com>
2022-03-11 15:05:49 -0600

    removed backgroun

--- src/components/About.js
+++ src/components/About.js
@@ -9,13 +9,13 @@ export default function About() {
             A little about me!
           </h1>
         </div>
-        <div className="px-10 py-20 md:flex-row flex-row items-center bg-gray-500">
+        <div className="px-10 py-20 md:flex-row flex-row items-center">
           <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2 px-20 py-20">
             <p className="text-base leading-relaxed lg:w-4/5 sm:mx-auto mx-2 text-gray-900">
             Over the past couple of years, I worked on getting myself comfortable
             with tech stack needed for frontend development. I am currently 
             expanding my into GitHub Actions and workflows. While at it, I am
-            also honing my product management skills :rocket:. 
+            also honing my product management skills.
             </p>
           </div>
         </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function About() {
   return (
     <section id="home" className="pb-20">
-      <div className="flex px-20 py-20 md:flex-row flex-row items-center bg-red-100">
+      <div className="flex px-20 py-20 md:flex-row flex-row items-center">
         <div className="lg:flex-row md:w-1/2 lg:pr-16 md:pr-16 md:items-start md:text-left mb-16 md:mb-0 py-20">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-green-300">
             Heya! I am Ramya
